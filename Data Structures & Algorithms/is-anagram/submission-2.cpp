5b746fdb067c6032f889261abe6062202cf03666
Hiral07-og <hgupta2_be24@thapar.edu>
2026-08-19 09:54:20 +0530

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+
+        int n = s.size();
+        int m = t.size();
+
+        if (m != n)
+            return false;
+
+        int count[26] = {0};
+
+        for (int i = 0; i < n; i++) {
+            count[s[i] - 'a']++;
+        }
+
+        for (int i = 0; i < m; i++) {
+            count[t[i] - 'a']--;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (count[i] != 0)
+                return false;
+        }
+
+        return true;
+    }
+};
